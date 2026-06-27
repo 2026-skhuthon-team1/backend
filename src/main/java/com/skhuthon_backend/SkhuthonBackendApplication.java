780fc334cbf8289dc2539871a3df21fa89f47d5e
Park, Seongjun <pysida70@gmail.com>
2026-06-27 13:31:23 +0900

    정확한 폴더로 다시 커밋

---
+++ src/main/java/com/skhuthon_backend/SkhuthonBackendApplication.java
@@ -0,0 +1,13 @@
+package com.skhuthon_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkhuthonBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkhuthonBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/skhuthon_backend/SkhuthonBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skhuthon_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkhuthonBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
